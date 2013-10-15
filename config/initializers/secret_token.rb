edab658d34309cfb1157226f23fe0da6781a4d4c
Exor <darkex@gmail.com>
2013-10-15 11:23:35 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuizGenerator::Application.config.secret_key_base = 'd775c1f4fc4cb4788a38f5e58567c771aee8422934646fef0ccac441c7918423bbf58053b6b619e97fa07308f46da1722a597003612d8dcc59f06e5923635e5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QuizGenerator::Application.config.session_store :cookie_store, key: '_quiz_generator_session'

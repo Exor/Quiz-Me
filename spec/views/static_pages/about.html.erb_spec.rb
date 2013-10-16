415b265d67dea94bf10b7d4efe5c44f7896a3c4b
Exor <darkex@gmail.com>
2013-10-16 12:10:46 +0900

    Update pages and links

--- spec/views/static_pages/about.html.erb_spec.rb
+++ spec/views/static_pages/about.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "about.html" do
 	subject { page }
-	before { visit '/static_pages/about' }
+	before { visit about_path }
 	
 	it { should have_title("Quiz | About") }
 end
--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "home.html" do
 	subject { page }
-	before { visit '/static_pages/home' }
+	before { visit root_path }
 
 	it { should have_title("Quiz | Home") }
 end

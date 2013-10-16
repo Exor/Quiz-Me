11171b3a924b162ac16a5bab4d08115a50ffc74f
Exor <darkex@gmail.com>
2013-10-16 13:31:05 +0900

    Username is case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
+	before_save { self.email = email.downcase } 
+	before_save { self.name = name.downcase }
 	validates :name, 
 		presence: true, 
 		length: { minimum: 6, maximum: 100 },
@@ -7,7 +8,7 @@ class User < ActiveRecord::Base
 		uniqueness: { case_sensitive: false }
 	validates :email, 
 		presence: true, 
-		format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+		format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i },
 		uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, length: { minimum: 8 }

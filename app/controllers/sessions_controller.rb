919bd9aa0ca797fb4548950f2ceb8abed0a74744
Exor <darkex@gmail.com>
2013-10-21 09:22:06 +0900

    Edit logged in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ def create
 	user = User.find_by(name: params[:session][:username].downcase)
 	if user && user.authenticate(params[:session][:password])
 		sign_in user
-		redirect_to root_path
+		redirect_to browse_path
 	else
 		flash.now[:error] = 'Invalid username/password combination'
 		render 'new'

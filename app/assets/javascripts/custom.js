7838e2d3a487c47cdd4b5c186cf862cd55f5b0ac
Exor <darkex@gmail.com>
2013-10-22 11:16:25 +0900

    Modify javascript for questions

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -12,22 +12,52 @@ function add_question(link){
 	strVar += "<input id=\"quiz_questions_attributes_";strVar += number;strVar += "__destroy\" name=\"quiz[questions_attributes][";strVar += number;strVar += "][_destroy]\" type=\"hidden\" value=\"false\" \/>";
 	strVar += "<a href=\"#\" onclick=\"remove_fields(this); return false;\">Remove Question<\/a>";
 	strVar += "<label for=\"quiz_questions_attributes_";strVar += number;strVar += "_category\">Type<\/label>";
-	strVar += "<select id=\"quiz_questions_attributes_";strVar += number;strVar += "_category\" name=\"quiz[questions_attributes][";strVar += number;strVar += "][category]\"><option value=\"tf\">True\/False<\/option>";
+	strVar += "<select id=\"quiz_questions_attributes_";strVar += number;strVar += "_category\" name=\"quiz[questions_attributes][";strVar += number;strVar += "][category]\" onChange=\"changeType(this)\">";
+	strVar += "<option value=\"\">-- Choose a Type --<\/option>";
+	strVar += "<option value=\"tf\">True\/False<\/option>";
 	strVar += "<option value=\"fill\">Fill in the Blank<\/option>";
 	strVar += "<option value=\"multi\">Multiple Choice<\/option><\/select>";
 	strVar += "<label for=\"quiz_questions_attributes_";strVar += number;strVar += "_content\">Question<\/label>";
 	strVar += "<input id=\"quiz_questions_attributes_";strVar += number;strVar += "_content\" name=\"quiz[questions_attributes][";strVar += number;strVar += "][content]\" type=\"text\" \/>";
-	strVar += "<label for=\"quiz_questions_attributes_";strVar += number;strVar += "_fill_answer\">Fill Answer<\/label>";
-	strVar += "<input id=\"quiz_questions_attributes_";strVar += number;strVar += "_fill_answer\" name=\"quiz[questions_attributes][";strVar += number;strVar += "][fill_answer]\" type=\"text\" \/>";
+	strVar += "<label class=\"fillanswer\" for=\"quiz_questions_attributes_";strVar += number;strVar += "_fill_answer\">Fill Answer<\/label>";
+	strVar += "<input class=\"fillanswer\" id=\"quiz_questions_attributes_";strVar += number;strVar += "_fill_answer\" name=\"quiz[questions_attributes][";strVar += number;strVar += "][fill_answer]\" type=\"text\" \/>";
+	strVar += "<p class=\"tfanswer\">"
 	strVar += "<label for=\"quiz_questions_attributes_";strVar += number;strVar += "_tf_answer\">True False Answer<\/label>";
 	strVar += "<input id=\"quiz_questions_attributes_";strVar += number;strVar += "_tf_answer_t\" name=\"quiz[questions_attributes][";strVar += number;strVar += "][tf_answer]\" type=\"radio\" value=\"t\" \/>True";
 	strVar += "<input id=\"quiz_questions_attributes_";strVar += number;strVar += "_tf_answer_f\" name=\"quiz[questions_attributes][";strVar += number;strVar += "][tf_answer]\" type=\"radio\" value=\"f\" \/>False";
-	strVar += "<label for=\"quiz_questions_attributes_";strVar += number;strVar += "_multiple_answer\">Multiple Choice Answer<\/label>";
-	strVar += "<input id=\"quiz_questions_attributes_";strVar += number;strVar += "_multiple_answer\" name=\"quiz[questions_attributes][";strVar += number;strVar += "][multiple_answer]\" type=\"text\" \/>";
+	strVar += "<\/p>"
+	strVar += "<label class=\"multianswer\" for=\"quiz_questions_attributes_";strVar += number;strVar += "_multiple_answer\">Multiple Choice Answer<\/label>";
+	strVar += "<input class=\"multianswer\" id=\"quiz_questions_attributes_";strVar += number;strVar += "_multiple_answer\" name=\"quiz[questions_attributes][";strVar += number;strVar += "][multiple_answer]\" type=\"text\" \/>";
 	strVar += "<label for=\"quiz_questions_attributes_";strVar += number;strVar += "_explaination\">Explanation<\/label>";
 	strVar += "<input id=\"quiz_questions_attributes_";strVar += number;strVar += "_explaination\" name=\"quiz[questions_attributes][";strVar += number;strVar += "][explaination]\" type=\"text\" \/>";
 	strVar += "<\/fieldset>";
 
 
 	$(link).before(strVar);
+
+	hideQuestions();
+}
+
+function changeType(select)
+{
+	hideQuestions();
+
+	if (select.selectedIndex==1)
+	{//True/False
+		$(select).nextAll(".tfanswer").show();
+	}
+	else if (select.selectedIndex==2)
+	{//Fill in the blank
+		$(select).nextAll(".fillanswer").show();
+	}
+	else if (select.selectedIndex==3)
+	{//Multiple Choice
+		$(select).nextAll(".multianswer").show();
+	}
+};
+
+function hideQuestions() {
+	$(".fillanswer").hide();
+	$(".multianswer").hide();
+	$(".tfanswer").hide();  
 }

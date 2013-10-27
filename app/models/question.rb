eacd2bbceab620b32d1aa47aea32c075379f0a17
Exor <darkex@gmail.com>
2013-10-27 18:54:09 +0900

    let question model have answer nested forms

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
 	belongs_to :quiz
 	has_many :answers, dependent: :destroy
+	accepts_nested_attributes_for :answers, allow_destroy: true, :reject_if => lambda { |a| a[:content].blank? }
 	validates :category, 
 				presence: true, 
 				format: {with: /(^tf\z)|(^fill\z)|(^multi\z)|(^multiList\z)/i }

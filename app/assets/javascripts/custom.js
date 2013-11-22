895358a952570d0082847aeead5c34e472340f6b
Exor <darkex@gmail.com>
2013-11-22 18:05:07 +0900

    Fix Javascript

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -7,7 +7,7 @@ function add_fields(link, association, content) {
     var new_id = new Date().getTime();
     var regex = new RegExp("new_" + association, "g");
     $(link).parent().before(content.replace(regex, new_id));
-    //$('#new-question-fields').modal('show');
+    hideAnswers();
 }
 
 
@@ -15,39 +15,58 @@ function changeType(select)
 {
 	if (select.selectedIndex==0)
 	{//Choose a type
-		$(select).nextAll(".answer_fields").children(".tfanswer").hide();
-		$(select).nextAll(".answer_fields").children(".tfanswer").children("input").prop('checked', false);
-		$(select).nextAll(".answer_fields").children(".fillanswer").hide();
-		$(select).nextAll(".answer_fields").children(".add_remove_answer_link").hide();
-		$(select).nextAll(".add_remove_answer_link").hide();
+		$(select).nextAll(".answer").hide();
+		$(select).nextAll(".add_answer_button").hide();
+
+		removeAllButFirstAnswer(select);
 	}
-	if (select.selectedIndex==1)
+	else if (select.selectedIndex==1)
 	{//True/False
-		$(select).nextAll(".answer_fields").children(".tfanswer").show();
-		$(select).nextAll(".answer_fields").children(".fillanswer").hide();
-		$(select).nextAll(".answer_fields").children(".add_remove_answer_link").hide();
-		$(select).nextAll(".add_remove_answer_link").hide();
+		$(select).nextAll(".answer").show();
+		$(select).nextAll(".add_answer_button").hide();
+		$(select).nextAll(".answer").children(".remove_answer_button").hide();
+
+		$(select).nextAll(".answer").children(".tfanswer").show();
+		$(select).nextAll(".answer").children(".fillanswer").hide();
+
+		removeAllButFirstAnswer(select);
 	}
 	else if (select.selectedIndex==2)
 	{//Fill in the blank
-		$(select).nextAll(".answer_fields").children(".fillanswer").show();
-		$(select).nextAll(".answer_fields").children(".tfanswer").hide();
-		$(select).nextAll(".answer_fields").children(".tfanswer").children("input").prop('checked', false);
-		$(select).nextAll(".answer_fields").children(".add_remove_answer_link").hide();
-		$(select).nextAll(".add_remove_answer_link").hide();
+		$(select).nextAll(".answer").show();
+		$(select).nextAll(".add_answer_button").hide();
+		$(select).nextAll(".answer").children(".remove_answer_button").hide();
+
+		$(select).nextAll(".answer").children(".fillanswer").show();
+		$(select).nextAll(".answer").children(".tfanswer").hide();
+		$(select).nextAll(".answer").children(".tfanswer").children("input").prop('checked', false);
+
+		removeAllButFirstAnswer(select);
 	}
 	else if (select.selectedIndex==3 || select.selectedIndex==4)
 	{//Multiple Choice
-		$(select).nextAll(".answer_fields").children(".fillanswer").show();
-		$(select).nextAll(".answer_fields").children(".tfanswer").hide();
-		$(select).nextAll(".answer_fields").children(".tfanswer").children("input").prop('checked', false);
-		$(select).nextAll(".answer_fields").children(".add_remove_answer_link").show();
-		$(select).nextAll(".add_remove_answer_link").show();
+		$(select).nextAll(".answer").show();
+		$(select).nextAll(".add_answer_button").show();
+		$(select).nextAll(".answer").children(".remove_answer_button").show();
+
+		$(select).nextAll(".answer").children(".fillanswer").show();
+		$(select).nextAll(".answer").children(".tfanswer").hide();
+		$(select).nextAll(".answer").children(".tfanswer").children("input").prop('checked', false);
 	}
+
+	hideDeletedAnswers(select);
 };
 
 function hideAnswers() {
 	var selectors = $('.questiontype')
 	for (i=0;i<selectors.length;i++)
 		{changeType(selectors[i]);}
+}
+
+function hideDeletedAnswers(select) {
+	$(select).nextAll(".answer").children("input[type=hidden][value=1]").parent().hide();
+}
+
+function removeAllButFirstAnswer(select) {
+	remove_fields($(select).nextAll(".answer:not(:first)").children("a"));
 }
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 QuizGenerator::Application.initialize!
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
+	if html_tag =~ /^<label/
+		errors = Array(instance_tag.error_message).join(',')
+		"#{html_tag}<div class='field_with_errors'>#{errors}</div>".html_safe
+	else
+		"#{html_tag}".html_safe
+	end
+  #{}"<span class='field_error'>#{html_tag}</span>".html_safe
+end

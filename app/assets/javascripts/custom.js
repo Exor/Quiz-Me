8791ff775a636a112ef86bf4d7f4063312356a3a
Exor <darkex@gmail.com>
2013-11-28 17:11:10 +0900

    Finish social media buttons

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -5,7 +5,6 @@ stLight.options({publisher: "88366333-b42c-4103-adcb-648d74c35fd4", doNotHash: t
 $(document).ready(initialize);
 $(document).on('page:load', initialize);
 
-
 function remove_fields(link) {
     $(link).prev("input[type=hidden]").val("1");
     $(link).closest(".fields").hide();

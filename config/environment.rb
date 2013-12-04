f3559809c0abe76feba565b3cfedd9d7327f0387
Exor <darkex@gmail.com>
2013-12-04 15:34:10 +0900

    Add styling to error messages

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,19 @@ require File.expand_path('../application', __FILE__)
 QuizGenerator::Application.initialize!
 
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
-	if html_tag =~ /^<label/
-		errors = Array(instance_tag.error_message).join(',')
-		"#{html_tag}<div class='field_with_errors'>#{errors}</div>".html_safe
-	else
-		"#{html_tag}".html_safe
-	end
+	class_attr_index = html_tag.index 'class="'
+
+  	if class_attr_index
+    	html_tag.insert class_attr_index+7, 'has-error '
+ 	else
+   		html_tag.insert html_tag.index('>'), ' class="has-error"'
+ 	end	
+
+	#if html_tag =~ /^<label/
+	#	errors = Array(instance_tag.error_message).join(',')
+	#	"#{html_tag}<div class='has-error'>#{errors}</div>".html_safe
+	#else
+	#	"#{html_tag}".html_safe
+	#end
   #{}"<span class='field_error'>#{html_tag}</span>".html_safe
 end

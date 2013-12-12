02171241070a3f177be5e1c4e63b350abc3486d6
Exor <darkex@gmail.com>
2013-12-12 12:45:10 +0900

    Fix bug with edit form

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,7 @@
 function initialize(){
 var switchTo5x=true;
-stLight.options({publisher: "88366333-b42c-4103-adcb-648d74c35fd4", doNotHash: true, doNotCopy: true, hashAddressBar: false});	
+stLight.options({publisher: "88366333-b42c-4103-adcb-648d74c35fd4", doNotHash: true, doNotCopy: true, hashAddressBar: false});
+dealWithQuestionNumbers();
 }
 
 $(document).ready(initialize);
